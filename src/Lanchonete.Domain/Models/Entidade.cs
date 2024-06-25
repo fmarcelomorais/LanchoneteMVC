9fa487df83df1e4440ee1bcc5c3536e3c87ae44c
fmarcelomorais <marcelomorais.program@gmail.com>
2024-06-25 10:33:47 -0300

    feat: Criado as classes de dominio

---
+++ src/Lanchonete.Domain/Models/Entidade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanchonete.Domain.Models
+{
+    public class Entidade
+    {
+        public Guid Id { get; set; }
+
+        public Entidade()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ src/Lanchonete.Domain/Models/Lanche.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanchonete.Domain.Models
+{
+    public class Lanche
+    {
+    }
+}
