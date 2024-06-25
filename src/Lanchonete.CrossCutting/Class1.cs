df5aba5bfd7ee9e2e278521139426c315b0ca6df
fmarcelomorais <marcelomorais.program@gmail.com>
2024-06-25 10:23:43 -0300

    feat: commit initial

---
+++ src/Lanchonete.CrossCutting/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Lanchonete.CrossCutting
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/Lanchonete.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lanchonete.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
